6b8d5191a289b3afdde72a1f096c7bd9547f735f
karunyasai932-stack <karunyasai932@gmail.com>
2026-03-01 20:53:50 +0530

    Initial commit project setup

---
+++ src/uc4.java
@@ -0,0 +1,21 @@
+UC4
+public class OopsBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] bannerLines = {
+
+            String.join("", "  *****          *****          *******         ******* "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *******          *******"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "  *****          *****         *                *******")
+        };
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
