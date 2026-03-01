46d8c9bfac24d123d60359a4513dcb12759b28a4
karunyasai932-stack <karunyasai932@gmail.com>
2026-03-01 21:03:28 +0530

    Initial commit project setup

---
+++ src/uc5.java
@@ -0,0 +1,31 @@
+UC5
+public class OopsBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] bannerLines = createBanner();
+        printBanner(bannerLines);
+
+    }
+
+    public static String[] createBanner() {
+
+        return new String[] {
+
+            String.join("", "  *****          *****          *******         ******* "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *******          *******"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "  *****          *****         *                *******")
+        };
+    }
+
+    public static void printBanner(String[] bannerLines) {
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
