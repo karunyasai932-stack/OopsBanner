1d91ef6c7fe9f51be5f2b188848915386aa74fe3
karunyasai932-stack <karunyasai932@gmail.com>
2026-03-10 13:31:04 +0530

    Create UC4.java

---
+++ UC4.java
@@ -0,0 +1,20 @@
+public class OopsBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] bannerLines = {
+
+            String.join("", "  *****          *****          *******         ******* "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *        *      *         "),
+            String.join("", "*       *      *       *       *******          *******"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "*       *      *       *       *                        *"),
+            String.join("", "  *****          *****         *                *******")
+        };
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
